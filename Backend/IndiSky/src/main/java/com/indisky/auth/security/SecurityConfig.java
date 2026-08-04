c94bcc7d7a4f5a42f4c8e2d3151ec040391d9a97
Omkar Borude <omkarborude15@gmail.com>
2026-08-04 18:52:10 +0530

    feature(auth): configure Spring Security

--- Backend/IndiSky/src/main/java/com/indisky/auth/security/SecurityConfig.java
+++ Backend/IndiSky/src/main/java/com/indisky/auth/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(customizer -> customizer.disable())
                 .authorizeHttpRequests(request -> request
-//                        .requestMatchers("/user/register","/user/login")
+//                        .requestMatchers("/user/register", "/user/login")
                         .requestMatchers("/**")
                         .permitAll()
 //                        .requestMatchers("/admin/**").hasRole("ADMIN")
