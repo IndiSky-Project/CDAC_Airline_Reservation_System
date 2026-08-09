48dd924e305ac4a803e56937285543c553c221e1
Sharad Tichkule <sharadtichkule2@gmail.com>
2026-08-09 15:27:35 +0530

    Added booking controller

--- Backend/IndiSky/src/main/java/com/indisky/user/controller/BookingController.java
+++ Backend/IndiSky/src/main/java/com/indisky/user/controller/BookingController.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+
 @RestController
 @RequestMapping("/api/bookings")
 @RequiredArgsConstructor
 public class BookingController {
+
 
     private final BookingService bookingService;
 
